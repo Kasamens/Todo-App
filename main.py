a81c63b19c65d5abb3bbdbd90e380ffa012f463e
Kojo Asamoa-Mensah <kwadwoasamoa7@gmail.com>
2022-01-05 22:18:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from fastapi import FastAPI
+from datetime import datetime
+from typing import Optional
+from model.model import Task,TaskList
+from fastapi.encoders import jsonable_encoder
+import model.taskman as taskman
+from fastapi.encoders import jsonable_encoder
+
+app = FastAPI()
+
+@app.get("/api/tasks")
+async def get_tasks():
+    return await taskman.get_tasks()
+
+@app.get("/api/tasks/{id}")
+async def get_task(id:int):
+    return await taskman.get_tasks(id)
+
+@app.post("/api/tasks/create")
+async def create_task(task: Task):
+    id = await taskman.create_task(task)
+    return await taskman.get_tasks(id)
+
+@app.put("/api/tasks/{id}/update")
+async def update_task(id:int, task:Task):
+    await taskman.update_task(id, task)
+    return await taskman.get_tasks(id)
+
+
+@app.delete("/api/tasks/{id}/delete")
+async def delete_task(id:int):
+    id = await taskman.delete_task(id)
+    response = {id: "Task successfully deleted"}
+    return jsonable_encoder(response)
